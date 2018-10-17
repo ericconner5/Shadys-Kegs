c25aace7777e48ff7fccf41a5f7bdb75f3ef6d76
Eric Conner and Kendra Kelly <eric@>
2018-10-17 11:19:09 -0700

    initialize project and get the hardcoded first keg to display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Keg } from './models/task.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'to-do';
+  // currentFocus: string = 'Angular Homework';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+  tapRoomName: string = "Shady's Kegs";
+  kegs: Keg[] = [
+    new Keg('Old Tavern Rat', 'Lompoc', 'Barleywine', 9.7, 95, 8, 120),
+  ];
+
+  selectedKeg = null;
+
+  finishedEditing() {
+    this.selectedKeg = null;
+  }
+
+  editKeg(clickedKeg) {
+    this.selectedKeg = clickedKeg;
+  }
+
+  abvColor(currentKeg){
+    if (currentKeg.abv >= 9){
+      return "bg-danger";
+    } else if ((currentKeg.abv <= 4) && (currentKeg.abv <= 8)) {
+      return  "bg-warning";
+    } else {
+      return "bg-success";
+    }
+  }
 }
---
+++ src/app/models/task.model.ts
@@ -0,0 +1,4 @@
+export class Keg {
+  public tapped: boolean = true;
+  constructor(public name: string, public brewery: string, public style: string, public abv: number, public ibu: number, public price: number, public pintCount: number) { }
+}
